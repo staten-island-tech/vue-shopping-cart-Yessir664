c659dd26b68376226571b46f246f9ea6c87a8f1f
Yessir664 <justin.zhang23@sitechhs.com>
2022-01-23 13:24:49 -0500

    commit

---
+++ project/src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="agent-card">
+    <h1 class="agent-name">{{ name }}</h1>
+    <img class="agent-icon" :src="image" />
+    <h2 class="agent-price">${{ price }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: String,
+    image: String,
+    price: Number,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.agent-icon {
+  width: 20rem;
+}
+.agent-card {
+  margin: 40px;
+  text-align: center;
+}
+</style>
