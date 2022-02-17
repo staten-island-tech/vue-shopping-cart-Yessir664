04c71055d657559b9ef446b5e1c4dd2151a15a07
Yessir664 <67617753+Yessir664@users.noreply.github.com>
2022-02-16 21:39:59 -0500

    alt

--- project/src/components/Card.vue
+++ project/src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-on:click="buyAgentClick(name)" class="agent-card">
     <h1 class="agent-name">{{ name }}</h1>
-    <img class="agent-icon" :src="image" />
+    <img class="agent-icon" :src="image" :alt="name"/>
   </div>
 </template>
 
